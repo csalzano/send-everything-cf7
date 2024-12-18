a7a9ace7f6da116ddc0814ce3b4bfc6f04b4b4a9
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-12-18 14:01:07 -0500

    Fixes "Domain Path" in plugin header comment. Adds a languages folder containing all current translations.

--- send-everything-cf7.php
+++ send-everything-cf7.php
@@ -7,7 +7,7 @@
  * Author URI: https://breakfastco.xyz
  * Version: 1.2.0
  * Text Domain: send-everything-cf7
- * Domain Path: languages
+ * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
